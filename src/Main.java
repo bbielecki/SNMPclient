a7ebb8a5a09b36bb69b447b5da3f7322fa41719c
Bartlomiej <bartlomiejbielecki1@gmail.com>
2016-12-03 13:14:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Bartłomiej on 03.12.2016.
+ */
+public class Main {
+
+    public static final void main(String args[]){
+
+
+    }
+}
---
+++ src/Viewer.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Bartłomiej on 03.12.2016.
+ */
+public class Viewer {
+}
